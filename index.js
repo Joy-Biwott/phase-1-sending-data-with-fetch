bb004daf24222de022e2f84ee7c28ab371cdac2e
Joy <biwott.joy.03@gmail.com>
2022-08-28 22:42:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+//Add your code here
+document.addEventListener('DOMContentLoaded', (e) =>{
+  e.preventDefault()
+
+
+
+function submitData(userName, userEmail){
+    const configMessage ={
+        method : "POST",
+        headers : {
+            "Content-Type":"application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name : `${userName}`,
+            email : `${userEmail}`,
+        })
+      }
+   
+    return fetch('http://localhost:3000/users', configMessage)
+    .then(res => (res.json()))
+      .then(function (data) {
+        document.body.innerHTML = data["id"];
+      })
+      .catch(function (error) {
+        alert("Bad things!");
+        document.body.innerHTML = error.message;
+      })
+    }    
+    submitData();
+ })
+
+
